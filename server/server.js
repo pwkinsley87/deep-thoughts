5ac6f4a9e43699e7e0586cb42005ee810402325d
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-09-03 10:43:48 -0500

    set up types and queries, connected to Apollo server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require('express');
+// import Apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL Schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express applicaton as middleware
+  server.applyMiddleware({ app });
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
