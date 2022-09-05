baaf827a596398dd27cf28d7ad7a7dfe4fd600c0
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-09-05 17:24:53 -0500

    added page navigation and user-based mutations

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,64 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
+   <ApolloProvider client={client}>
+    <Router>
+     <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
       <div className='container'>
-        <Home />
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup />}
+          />
+          <Route path="/profile">
+            <Route path=":username" element={<Profile />} />
+            <Route path="" element={<Profile />} />
+          </Route>        
+          <Route
+            path="/thought"
+            element={<SingleThought />}
+          />
+          <Route 
+            path="*"
+            element={<NoMatch />}
+          />
+        </Routes>
       </div>
       <Footer />
-    </div>
+      </div>
+    </Router>
+    </ApolloProvider>
   );
 }
 
